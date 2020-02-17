ab7729cbaf820cbbc16ebca9ac2cfd99aa46ec56
codawalker <fierymail@list.ru>
2020-02-17 13:00:07 +0300

    Реализованы два метода по поиску калорий превышающих дневной уровень.

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -6,6 +6,7 @@ import ru.javawebinar.topjava.util.UserMealsUtil;
 public class Main {
     public static void main(String[] args) {
         UserMealsUtil.main(args);
+
         System.out.format("Hello TopJava Enterprise!");
     }
 }
